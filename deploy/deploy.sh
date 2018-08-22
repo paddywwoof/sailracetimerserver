1fd87c994517afb6c05ee476fc3a327947e679a0
Martin Ashby <martin@ashbysoft.com>
2018-08-22 22:20:47 +0100

    Changes for sample deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,4 +3,3 @@ docker-compose build
 docker-compose push
 hyper compose pull -f hyper-compose.yml
 hyper compose up -f hyper-compose.yml -d --force-recreate
-hyper compose logs -f hyper-compose.yml -d --force-recreate
